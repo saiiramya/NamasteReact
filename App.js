09e8ff3fb3591b31a99495bb64372087f14a9d1d
saiiramya <nsaiiramya@gmail.com>
2025-05-19 15:28:11 +0530

    ep 3

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom/client"
+import ReactDOM from "react-dom/client";
 
 {
   /* <div>
@@ -14,19 +14,35 @@ import ReactDOM from "react-dom/client"
 </div> */
 }
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am h1"),
-    React.createElement("h2", {}, "I am h2"),
-  ]),
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am h1"),
-    React.createElement("h2", {}, "I am h2"),
-  ]),
-]);
+// const parent = React.createElement("div", { id: "parent" }, [
+//   React.createElement("div", { id: "child" }, [
+//     React.createElement("h1", {}, "I am h1"),
+//     React.createElement("h2", {}, "I am h2"),
+//   ]),
+//   React.createElement("div", { id: "child" }, [
+//     React.createElement("h1", {}, "I am h1"),
+//     React.createElement("h2", {}, "I am h2"),
+//   ]),
+// ]);
 
-console.log(parent);
+// console.log(parent);
 
-const heading = React.createElement("h1", {}, "Hello World!");
+// const heading = React.createElement("h1", {}, "Hello World!");
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(parent);
+
+// episode 3
+
+const Title = () => {
+  return <h1>Namaste react using JSX </h1>;
+};
+
+const HeadingComponent = () => (
+  <div>
+    <Title />
+    <h1>Heading Component</h1>
+  </div>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<HeadingComponent />);
--- README.md
+++ README.md
@@ -20,3 +20,6 @@
  *
  * Transitive Dependencies
  */
+
+
+ component composition
